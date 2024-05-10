d392d641cbe6c95f8e8f47d44e621b5eb9c3c75c
devinigor <devinigor@icloud.com>
2024-05-10 22:30:57 +0300

    app init

---
+++ FurnitureApp/FurnitureApp/FurnitureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureAppApp.swift
+//  FurnitureApp
+//
+//  Created by Игорь Девин on 10.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
